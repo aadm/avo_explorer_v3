047b0f355f037402ae5ab5e0b5d179657f6979e5
Alessandro Amato del Monte <alessandro.adm@gmail.com>
2024-07-10 09:35:55 +0000

    Jul 10, 2024, 11:35 AM

--- avo_explorer_v4.py
+++ avo_explorer_v4.py
@@ -16,30 +16,107 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import altair as alt
+
+
+def pr(vp, vs):
+    '''
+    Calculate Poisson's ratio. (aadm 2017)
+    '''
+    return (vp**2-2*vs**2) / (2*(vp**2-vs**2))
+
+
+def hilterman(vp1, vs1, rho1, vp2, vs2, rho2, theta,
+         angcontrib=False):
+    '''
+    Calculate P-wave reflectivity with Verm & Hilterman's equation. (aadm 2024)
+
+    Hilterman (2001), Seismic Amplitude Interpretation,
+    SEG Distinguished Instructor Short Course n.4 (p. 3-35)
+    '''
+    a = np.deg2rad(theta)
+    pr1 = pr(vp1, vs1)
+    pr2 = pr(vp2, vs2)
+    dvp = vp2 - vp1
+    drho = rho2 - rho1
+    dpr = pr2 - pr1
+
+    # calculate average properties
+    avp = np.mean([vp1, vp2], axis=0)
+    avs = np.mean([vs1, vs2], axis=0)
+    arho = np.mean([rho1, rho2], axis=0)
+    apr = np.mean([pr1, pr2], axis=0)
+ 
+    R0 = 0.5 * (dvp/avp + drho/arho) # normal incidence reflection coefficient
+
+    C = 4 * avs**2/avp**2 * np.sin(a)**2
+    term1 = R0 * (1 - 4 * avs**2/avp**2 * np.sin(a)**2)
+    term2 = dpr/(1 - apr)**2 * np.sin(a)**2
+    term3 = 0.5 * dvp/avp * (np.tan(a)**2 - 4 * avs**2/avp**2 * np.sin(a)**2)
+    R = term1 + term2 + term3
+
+    if angcontrib:
+        return R, term1, term2, term3
+    else:
+        return R
+
+
+def shuey(vp1, vs1, rho1, vp2, vs2, rho2, theta,
+           approx=True, terms=False, angcontrib=False):
+    '''
+    Calculate P-wave reflectivity with Shuey's equation. (aadm 2024)
+
+    Mavko et al. (2009), The Rock Physics Handbook, 2nd ed.
+      Cambridge University Press (p.102)
+    Castagna (1993), AVO Analysis - Tutorial and Review in
+      "Offset-Dependent Reflectivity: Theory and Practice of AVO Analysis",
+      ed. J. P. Castagna and M. Backus, SEG Investigations in Geophysics n.8 (p.9)
+    '''
+    a = np.deg2rad(theta)
+    pr1 = pr(vp1, vs1)
+    pr2 = pr(vp2, vs2)
+    dvp = vp2 - vp1
+    drho = rho2 - rho1
+    dpr = pr2 - pr1
+
+    # calculate average properties
+    avp = np.mean([vp1, vp2], axis=0)
+    arho = np.mean([rho1, rho2], axis=0)
+    apr = np.mean([pr1, pr2], axis=0)
+
+    R0 = 0.5*(dvp/avp + drho/arho) # normal incidence reflection coefficient
+
+    B = (dvp/avp) / (dvp/avp + drho/arho)
+    A0 = B - 2*(1 + B) * (1 - 2*apr)/(1 - apr) 
+    G = A0*R0 + dpr / (1 - apr)**2 # gradient: intermediate angles
+
+    F = 0.5 * dvp/avp # F: dominant approaching critical angles
+
+    # if angles _and_ velocities are array reshape
+    if isinstance(theta, np.ndarray) & isinstance(vp1, np.ndarray):
+        R0 = R0.reshape(-1, 1)
+        G = G.reshape(-1, 1)
+        F = F.reshape(-1, 1)
+
+    R = R0 + G*np.sin(a)**2 + F*(np.tan(a)**2 - np.sin(a)**2)
+
+    if approx:
+        R = R0 + G*np.sin(a)**2
+    else:
+        R = R0 + G*np.sin(a)**2 + F*(np.tan(a)**2 - np.sin(a)**2)
+
+    if terms:
+        if angcontrib:
+            return R, R0, G*np.sin(a)**2, F*(np.tan(a)**2 - np.sin(a)**2)
+        else:
+            return R, R0, G
+    else:
+        return R    
+
 
 def get_avo_classes(hybrid_class_4=True):
     '''
-    Returns reference AVO classes definition
-    as 3 Pandas DataFrames.
-
-    Parameters
-    ----------
-    hybrid_class_4 : bool, optional
-        If True, returns alternative class 4 using Castagna's
-        shale parameters coupled with Hilterman's class 3 sands.
-        Default: True
-
-    Returns
-    -------
-    shale : DataFrame
-    sand_brine : DataFrame
-    sand_gas : DataFrame
-
-    Notes
-    -----
-    Classes 1, 2, 3 from Hilterman, 2001,
-    Seismic Amplitude Interpretation,
+    Returns reference AVO classes definition from 
+    from Hilterman, 2001, Seismic Amplitude Interpretation,
     SEG-EAGE Distinguished Instructor Short Course.
     Class 4 from Castagna, J. P., and H. W. Swan, 1997,
     Principles of AVO crossplotting, The Leading Edge.
@@ -67,104 +144,6 @@ def get_avo_classes(hybrid_class_4=True):
     return shale, sandb, sandg
 
 
-def shuey(vp1, vs1, rho1, vp2, vs2, rho2, theta, approx=True, terms=False):
-    '''
-    Calculate P-wave reflectivity with Shuey's equation.
-    (aadm 2016, 2020)
-
-    Parameters
-    ----------
-    vp1, vs1, rho1 : float or array_like
-        P-, S-wave velocity (m/s) and density (g/cm3) of upper medium.
-    vp2, vs2, rho2 : float or array_like
-        P-, S-wave velocity (m/s) and density (g/cm3) of lower medium.
-    theta : int or array_like
-        Angle of incidence (degree).
-    approx : bool, optional
-        If True returns approximate 2-terms form. Default: True
-    terms :  bool, optional
-        If True returns reflectivity, intercept and gradient.
-        Default: False.
-
-    Returns
-    -------
-    R : float or array_like
-        Reflectivity at angle theta.
-    R0, G : float
-        Intercept and gradient, only output if terms is True.
-
-    Notes
-    -----
-    If input properties are arrays with length n
-    and angles are also arrays with length m,
-    the function returns a (n, m) array.
-
-    References
-    ----------
-    Avseth et al. (2005), Quantitative Seismic Interpretation,
-    Cambridge University Press (p.182)
-    '''
-    a = np.radians(theta)
-    dvp = vp2-vp1
-    dvs = vs2-vs1
-    drho = rho2-rho1
-    vp = np.mean([vp1, vp2], axis=0)
-    vs = np.mean([vs1, vs2], axis=0)
-    rho = np.mean([rho1, rho2], axis=0)
-    R0 = 0.5*(dvp/vp + drho/rho)
-    G = 0.5*(dvp/vp) - 2*(vs**2/vp**2)*(drho/rho+2*(dvs/vs))
-    F = 0.5*(dvp/vp)
-    # # if angles is an array
-    # if a.size > 1:
-    #     R0 = R0.reshape(-1, 1)
-    #     G = G.reshape(-1, 1)
-    #     F = F.reshape(-1, 1)
-    if approx:
-        R = R0 + G*np.sin(a)**2
-    else:
-        R = R0 + G*np.sin(a)**2 + F*(np.tan(a)**2-np.sin(a)**2)
-    if terms:
-        return R, R0, G
-    else:
-        return R
-
-
-def akirichards(vp1, vs1, rho1, vp2, vs2, rho2, theta):
-    '''
-    Calculates P-wave reflectivity with Aki-Richards equation.
-    (aadm 2017)
-
-    Parameters
-    ----------
-    vp1, vs1, rho1 : float or array_like
-        P-, S-wave velocity (m/s) and density (g/cm3) of upper medium.
-    vp2, vs2, rho2 : float or array_like
-        P-, S-wave velocity (m/s) and density (g/cm3) of lower medium.
-    theta : int or array_like
-        Angle of incidence (degree).
-
-    Returns
-    -------
-    R : float or array_like
-        Reflectivity at angle theta.
-
-    References
-    ----------
-    Mavko et al. (2009), The Rock Physics Handbook, Cambridge University Press (p.182)
-    '''
-    a = np.radians(theta)
-    p = np.sin(a)/vp1
-    dvp = vp2-vp1
-    dvs = vs2-vs1
-    drho = rho2-rho1
-    vp  = np.mean([vp1,vp2])
-    vs  = np.mean([vs1,vs2])
-    rho = np.mean([rho1,rho2])
-    A = 0.5*(1-4*p**2*vs**2)*drho/rho
-    B = 1/(2*np.cos(a)**2) * dvp/vp
-    C = 4*p**2*vs**2*dvs/vs
-    R = A + B - C
-    return R
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -172,15 +151,15 @@ def akirichards(vp1, vs1, rho1, vp2, vs2, rho2, theta):
 
 st.set_page_config(page_title='AVO Explorer', layout="centered")
 
-with st.sidebar:
-    st.title(':grey[AVO Explorer v3]')
-    st.write(
-        '''
-        Porting of my old
-        [AVO Explorer notebook](https://github.com/aadm/avo_explorer).
+# with st.sidebar:
+#     st.title(':grey[AVO Explorer v4]')
+#     st.write(
+#         '''Porting of my old
+#         [AVO Explorer notebook](https://github.com/aadm/avo_explorer).
 
-        (aadm 2019,2023)
-        ''')
+#         (aadm 2019, 2023, 2024)''')
+
+st.title(':grey[AVO Explorer v4]')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # widget: input elastic properties for shale and sand 
